e122b59e55268b1414d6439d2100a6bca129ccc2
Eric Celerin <Ecceleri@mtu.edu>
2016-01-20 23:19:27 -0500

    Building Custom Controller

---
+++ Assets/Scripts/Entities/PlayerEntity.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+	private CharacterController characterController;
+	public bool isGrounded;
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		IsGrounded ();
+	}
+	void IsGrounded () {
+		//start pos
+		//direction
+		//length
+		isGrounded = (Physics.Raycast(transform.position, -transfrom.up, characterController.height / 1.8F));
+
+	}
+}
