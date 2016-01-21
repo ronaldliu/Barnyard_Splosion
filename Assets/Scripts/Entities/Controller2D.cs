24e9681797d09921b2de4227a076d93479157548
Eric Celerin <Ecceleri@mtu.edu>
2016-01-21 05:57:38 -0500

    Controller fine tuning

--- Assets/Scripts/Entities/Controller2D.cs
+++ Assets/Scripts/Entities/Controller2D.cs
@@ -7,6 +7,9 @@ public class Controller2D : MonoBehaviour {
 	public int horizontalRayCount = 4;
 	public int verticalRayCount = 4;
 
+	float maxClimbAngle = 80;
+	float maxDescendAngle = 75;
+
 	public LayerMask collisionMask;
 
 	BoxCollider2D collider;
@@ -27,7 +30,10 @@ public class Controller2D : MonoBehaviour {
 	public void Move(Vector3 velocity){
 		UpdateRaycastOrigins ();
 		collisions.Reset ();
-
+		collisions.velocityOld = velocity;
+		if (velocity.y < 0) {
+			DescendSlope (ref velocity);
+		}
 		if (velocity.x != 0) {
 			HorizonatalCollisions (ref velocity);
 		}
@@ -48,12 +54,33 @@ public class Controller2D : MonoBehaviour {
 			Debug.DrawRay (rayOrigin, Vector2.right * directionX * rayLength, Color.red);
 
 			if (hit) {
-				velocity.x = (hit.distance - skinWidth) * directionX;
-				rayLength = hit.distance;
+				float slopeAngle = Vector2.Angle (hit.normal, Vector2.up);
+				if (i == 0 && slopeAngle <= maxClimbAngle) {
+					if (collisions.descendingSlope) {
+						collisions.descendingSlope = false;
+						velocity = collisions.velocityOld;
+					}
+					float distanceTpSlopeStart = 0;
+					if (slopeAngle != collisions.slopeAngleOld) {
+						distanceTpSlopeStart = hit.distance - skinWidth;
+						velocity.x -= distanceTpSlopeStart * directionX;
+					}
+					ClimbSlope (ref velocity, slopeAngle);
+					velocity.x += distanceTpSlopeStart * directionX;
 
-				collisions.left = directionX == -1;
-				collisions.right = directionX == 1;
+				}
+				if (!collisions.climbingSlope || slopeAngle > maxClimbAngle) {
 
+					velocity.x = (hit.distance - skinWidth) * directionX;
+					rayLength = hit.distance;
+
+					if(collisions.climbingSlope){
+						velocity.y = Mathf.Tan (collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs (velocity.x);
+					}
+
+					collisions.left = directionX == -1;
+					collisions.right = directionX == 1;
+				}
 			}
 		}
 	}
@@ -72,10 +99,64 @@ public class Controller2D : MonoBehaviour {
 				velocity.y = (hit.distance - skinWidth) * directionY;
 				rayLength = hit.distance;
 
+				if (collisions.climbingSlope) {
+					velocity.x = velocity.y / Mathf.Tan (collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign (velocity.x);
+				}
+
 				collisions.below = directionY == -1;
 				collisions.above = directionY == 1;
 			}
 		}
+		if (collisions.climbingSlope) {
+			float directionX = Mathf.Sign (velocity.x);
+			rayLength = Mathf.Abs (velocity.x) + skinWidth;
+			Vector2 rayOrigin = ((directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight) +Vector2.up*velocity.y;
+			RaycastHit2D hit = Physics2D.Raycast (rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+			if (hit) {
+				float slopeAngle = Vector2.Angle (hit.normal, Vector2.up);
+				if (slopeAngle != collisions.slopeAngle) {
+					velocity.x = (hit.distance - skinWidth) * directionX;
+					collisions.slopeAngle = slopeAngle;
+				}
+			}
+		}
+	}
+	void ClimbSlope(ref Vector3 velocity, float slopeAngle){
+
+		float moveDistance = Mathf.Abs (velocity.x);
+		float climbVelocityY = Mathf.Sin (slopeAngle * Mathf.Deg2Rad) * moveDistance;
+		if (velocity.y <= climbVelocityY) {
+			velocity = new Vector3 (Mathf.Cos (slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign (velocity.x), climbVelocityY);
+			collisions.below = true;
+			collisions.climbingSlope = true;
+			collisions.slopeAngle = slopeAngle;
+		}
+
+	}
+
+	void DescendSlope(ref Vector3 velocity){
+		float directionX = Mathf.Sign (velocity.x);
+		Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomRight : raycastOrigins.bottomLeft;
+		RaycastHit2D hit = Physics2D.Raycast (rayOrigin, -Vector2.up, Mathf.Infinity, collisionMask);
+		if (hit) {
+			float slopeAngle = Vector2.Angle (hit.normal, Vector2.up);
+			if (slopeAngle != 0 && slopeAngle <= maxDescendAngle) {
+				if (Mathf.Sign(hit.normal.x) == directionX) {
+					if(hit.distance -skinWidth <= Mathf.Tan(slopeAngle *Mathf.Deg2Rad) *Mathf.Abs(velocity.x)){
+						float moveDistance = Mathf.Abs(velocity.x);
+						float descendVelocityY = Mathf.Sin (slopeAngle * Mathf.Deg2Rad) * moveDistance;
+						velocity.x = Mathf.Cos (slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign (velocity.x);
+						velocity.y -= descendVelocityY;
+
+						collisions.slopeAngle = slopeAngle;
+						collisions.descendingSlope = true;
+						collisions.below = true;
+					}
+				}
+			}
+
+		}
+
 	}
 	void UpdateRaycastOrigins(){
 		Bounds bounds = collider.bounds;
@@ -104,9 +185,16 @@ public class Controller2D : MonoBehaviour {
 		public bool above, below;
 		public bool left, right;
 
+		public bool climbingSlope,descendingSlope;
+		public float slopeAngle, slopeAngleOld;
+		public Vector3 velocityOld;
+
 		public void Reset(){
 			above = below = false;
 			left = right = false;
+			climbingSlope = descendingSlope = false;
+			slopeAngleOld = slopeAngle;
+			slopeAngle = 0;
 		}
 	}
 }
--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -3,14 +3,26 @@ using System.Collections;
 
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour {
-	float gravity = -20;
-	float moveSpeed = 6;
+	public float jumpHeight = 4;
+	public float timeToJumpApex = .4f;
+	public float moveSpeed = 6;
+	public float accelerationTimeAirborne = .2f;
+	public float accelerationTimeGrounded = .1f;
+
+	
+	float gravity;
+	float jumpVelocity;
 	Vector3 velocity;
+	float velocityXSmoothing;
+
 	Controller2D controller;
+
 	void Start () {
 		controller = GetComponent<Controller2D> ();
+		UpdateGravity ();
 	}
 	void Update(){
+		UpdateGravity ();
 
 		if (controller.collisions.above || controller.collisions.below) {
 			velocity.y = 0;
@@ -19,10 +31,20 @@ public class Player : MonoBehaviour {
 
 		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
 
-		velocity.x = input.x * moveSpeed;
+		if (Input.GetButtonDown ("Jump") && controller.collisions.below) {
+			velocity.y = jumpVelocity;
+		}
+
+		float targetVelocityX = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
 		velocity.y += gravity * Time.deltaTime;
 		controller.Move (velocity * Time.deltaTime);
 
 	}
+	public void UpdateGravity(){
+		gravity = -(jumpHeight * 2) / Mathf.Pow (timeToJumpApex, 2);
+		jumpVelocity = Mathf.Abs (gravity) * timeToJumpApex;
+		//print("Gravity: " + gravity + " Jump Vel: " +jumpVelocity);
 
+	}
 }
