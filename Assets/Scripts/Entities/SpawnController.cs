4cb6efc3de2e814b4e034413c763e62a957f966d
unknown <ronaldl@skyrim.lab.huskygames.com>
2016-01-31 16:46:11 -0500

    Getting the Spawner working

---
+++ Assets/Scripts/Entities/SpawnController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnController : MonoBehaviour {
+
+	public GameObject weapon;
+	public int spawnHeight;
+	// Use this for initialization
+	void Start () 
+	{
+		SpawnWeapon ();
+	}
+
+	void SpawnWeapon()
+	{
+		Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y + spawnHeight, transform.position.z);
+		Instantiate (weapon, spawnPosition, Quaternion.identity);
+	}
+}
