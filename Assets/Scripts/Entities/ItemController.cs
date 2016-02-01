d22c9c2e950096278e5ca90897cda32a8612a88d
Myfi <ronaldl@mtu.edu>
2016-01-31 19:44:50 -0500

    ItemController modifier

--- Assets/Scripts/Entities/ItemController.cs
+++ Assets/Scripts/Entities/ItemController.cs
@@ -3,19 +3,31 @@ using System.Collections;
 
 public class ItemController : MonoBehaviour {
 
-	public bool grabable = true;
-	public bool active = false;
+	Player wielder;
+	bool grabable = true;
+	bool active = false;
+
 	public int ammo;
 	public float firerate;
 	public GameObject projectile;
 	public float pStartX;
 	public float pStartY;
+	public float damage;
+	public float projectileSpeed;
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Player") 
 		{
-			// Attach the gun onto player
+			wielder = other.gameObject;
+			active = true;
+			grabable = false;
 		}
+	}
+
+	void Fire()
+	{
+		Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y + spawnHeight, transform.position.z);
+		Instantiate (projectile, spawnPosition, Quaternion.identity);
 	}
 }
