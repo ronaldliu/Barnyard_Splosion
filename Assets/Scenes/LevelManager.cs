a7d4e7ba5aa3555dc36d706aa3d6670c1a49ed00
unknown <magryzwa@bioshock.lab.huskygames.com>
2016-01-31 20:35:05 -0500

    Menu working -ish

---
+++ Assets/Scenes/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public Transform mainMenu, optionsMenu;
+
+	// Use this for initialization
+	public void LoadScene(string name){
+		Application.LoadLevel(name);
+	}
+	public void QuitGame(){
+		Application.Quit();
+	}
+	public void OptionsMenu(bool clicked){
+		if (clicked == true) {
+			optionsMenu.gameObject.SetActive (clicked);
+			mainMenu.gameObject.SetActive (false);
+		} else {
+			optionsMenu.gameObject.SetActive (clicked);
+			mainMenu.gameObject.SetActive (true);
+		}
+	}
+
+
+}
