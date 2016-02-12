346ae6f23f21466d5d330b01ef118432914a9c5f
Eric Celerin <Ecceleri@mtu.edu>
2016-02-12 13:04:51 -0500

    Heiarchy

--- Assets/Scripts/Entities/Item.cs
+++ Assets/Scripts/Entities/Item.cs
@@ -4,10 +4,25 @@ using System.Collections;
 [RequireComponent (typeof (WeaponController))]
 public class Item : MonoBehaviour {
 	Vector3 position;
+	Vector3 rotation;
+	bool held;
+	Player holdingMe;
 	void Start () {
-		
+	
 	}
 	void Update(){
+		if (held) {
 
+		} else {
+
+		}
+	}
+	public void CatchPlayer(Player me){
+		holdingMe = me;
+		held = true;
+	}
+	public void PlayerDrop(){
+		holdingMe = null;
+		held = false;
 	}
 }
