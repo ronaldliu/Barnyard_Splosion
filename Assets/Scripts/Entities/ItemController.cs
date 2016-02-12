b5101c23c14c0b28c7278180e533644c325a81a6
Ronald Liu <ronaldl@mtu.edu>
2016-02-11 20:29:45 -0500

    Assualtrifle now has autofire/ variable spread/ ammo

--- Assets/Scripts/Entities/ItemController.cs
+++ Assets/Scripts/Entities/ItemController.cs
@@ -8,13 +8,16 @@ public class ItemController : RaycastController {
 	bool active = false;
 
 	public int ammo;
+	// Higher the number the faster the gun shoots
 	public float firerate;
-	public GameObject projectile;//Move Weapon specific variable into a weapon script thet extends item!
+	// The Bullet trail
+	public GameObject projectile;
 	public Transform shotSpawn;
-	public float pStartX;
-	public float pStartY;
 	public float damage;
 	public float projectileSpeed;
+	// Used for bullet spread so it isnt just straight
+	// Higher the number the more variance
+	public float variance;
 
 	float nextfire;
 
@@ -31,15 +34,17 @@ public class ItemController : RaycastController {
 	{
 		if (firerate == 0) 
 		{
-			if (Input.GetButtonDown ("Fire_P1")) 
+			if (Input.GetButtonDown ("Fire_P2") && ammo != 0) 
 			{
 				Fire ();
+				ammo--;
 			}
 		} else {
-			if (Input.GetButton ("Fire_P2") && Time.time > nextfire) 
+			if (Input.GetButton ("Fire_P2") && Time.time > nextfire && ammo != 0) 
 			{
-				nextfire = Time.time + firerate;
+				nextfire = Time.time + 1/firerate;
 				Fire ();
+				ammo--;
 			}
 		}
 	}
@@ -57,14 +62,18 @@ public class ItemController : RaycastController {
 	void Fire()
 	{
 		// Instantiate (projectile, shotSpawn.position, shotSpawn.rotation);
-		Vector2 mousePos = new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y);
+		// Vector2 mousePos = new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y);
 		Vector2 shotSpawnPos = new Vector2 (shotSpawn.position.x, shotSpawn.position.y);
-		RaycastHit2D hit = Physics2D.Raycast (shotSpawnPos, mousePos - shotSpawnPos, 100, collisionMask);
-		Debug.DrawLine (shotSpawnPos, (mousePos - shotSpawnPos) * 100, Color.green);
+		Vector2 shotForward = new Vector2 (shotSpawn.right.x, shotSpawn.right.y + Random.Range (-variance/100, variance/100));
+		RaycastHit2D hit = Physics2D.Raycast (shotSpawnPos, shotForward, 100, collisionMask);
+
+		Debug.DrawLine (shotSpawnPos, shotForward * 100, Color.cyan);
 
 		if (hit) {
 			Player enemy = hit.transform.GetComponent<Player>(); //Create Player Dictionary
 			enemy.health -= damage;
 		}
 	}
+
+
 }
